0431ef03851e33217e9e323ea722f7c15768f5d0
Aaron <aarongillies88@gmail.com>
2020-05-27 21:17:39 -0400

    update readme, fixed var declaration in twoSum

--- JavaScript/twoSum.js
+++ JavaScript/twoSum.js
@@ -1,6 +1,6 @@
 // Return the 2 values in input array that add up to the target input
 
-var twoSum = function (nums, target) {
+const twoSum = function (nums, target) {
   // for(let i = 0; i < nums.length; i++) {  <----- Brute force solution, very slow as input size increase. O(n^2)
   //    for(let x = i + 1; x < nums.length; x++) {
   //        if(nums[i] + nums[x] === target) {
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code-challenges
+
+## Just a repository for code challenges as I complete them, maybe they will help someone else. Cheers!
