c7c5f7fa47a3a7539d50a7846957f44bf995ba18
Aaron <aarongillies88@gmail.com>
2020-05-29 20:29:13 -0400

    fixed maxScore problem

--- JavaScript/MaxScore.js
+++ JavaScript/MaxScore.js
@@ -1,15 +1,27 @@
-// function maximumScore(tileHand) {
-//   scoreData = [];
-//   tileHand.map((data) => {
-//     for (let [key, value] of Object.entries(data)) {
-//       if (Number.isInteger(value) === true) {
-//         scoreData.push(value);
-//       }
-//     }
-//   });
-//   const reducer = (acc, cV) => acc + cV;
-//   let result = scoreData.reduce(reducer);
-//   return result;
-// }
+// Add values in nested objects together and return total
 
-// console.log(maximumScore([1, 5, 7, 9, 15]));
+function maximumScore(tileHand) {
+  scoreData = [];
+  tileHand.map((data) => {
+    for (let [key, value] of Object.entries(data)) {
+      if (Number.isInteger(value) === true) {
+        scoreData.push(value);
+      }
+    }
+  });
+  const reducer = (acc, cV) => acc + cV;
+  let result = scoreData.reduce(reducer);
+  return result;
+}
+
+console.log(
+  maximumScore([
+    { tile: "N", score: 1 },
+    { tile: "K", score: 5 },
+    { tile: "Z", score: 10 },
+    { tile: "X", score: 8 },
+    { tile: "D", score: 2 },
+    { tile: "A", score: 1 },
+    { tile: "E", score: 1 },
+  ])
+);
