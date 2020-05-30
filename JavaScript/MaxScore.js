1cf47ed23c9c4f0de08814f84c2f7c8dacba32f9
Aaron <aarongillies88@gmail.com>
2020-05-29 20:27:37 -0400

    added maximumScore (not solved)

---
+++ JavaScript/MaxScore.js
@@ -0,0 +1,15 @@
+// function maximumScore(tileHand) {
+//   scoreData = [];
+//   tileHand.map((data) => {
+//     for (let [key, value] of Object.entries(data)) {
+//       if (Number.isInteger(value) === true) {
+//         scoreData.push(value);
+//       }
+//     }
+//   });
+//   const reducer = (acc, cV) => acc + cV;
+//   let result = scoreData.reduce(reducer);
+//   return result;
+// }
+
+// console.log(maximumScore([1, 5, 7, 9, 15]));
