ba1bb4ed7339d5d1d5aeae10eab31a15f47b7d48
Aaron <aarongillies88@gmail.com>
2020-06-03 18:59:57 -0400

    added solution for string palindrone checker

---
+++ JavaScript/isPalindrome.js
@@ -0,0 +1,17 @@
+// Determine whether or not a string is a palindrome or not.
+
+function isPalindrome(string) {
+  // Convert string to lowercase to account for potential mixed casing
+  string = string.toLowerCase();
+  // Reverse the string (split first, then run reverse on the array of chars, then join back together)
+  reversedString = string.split("").reverse().join("");
+  // Compare the reversed string with the original input string for equality
+  // If they are the same, return true. Otherwise return false
+  if (reversedString === string) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("Anna"));
