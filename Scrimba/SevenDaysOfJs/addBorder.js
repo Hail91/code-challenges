4fb6f4c151940f69c36f33fe68306137257d5d84
Aaron <aarongillies88@gmail.com>
2020-06-06 16:25:08 -0400

    added second somewhat cleaner more adaptable solution

--- Scrimba/SevenDaysOfJs/addBorder.js
+++ Scrimba/SevenDaysOfJs/addBorder.js
@@ -13,4 +13,20 @@ function addBorder(array) {
   return newArr;
 }
 
+// ** Cleaner solution, but I'm not a big fan of line 19 **
+
+// function addBorder(array) {
+//     //  write code here.
+//     const wall = '*'.repeat(array[0].length + 2);
+
+//     array.unshift(wall);
+//     array.push(wall);
+
+//     for(let i = 1; i < array.length - 1; i++) {
+//         array[i] = '*'.concat(array[i], '*');
+//     }
+
+//     return array;
+// }
+
 console.log(addBorder(["abc", "ded"]));
