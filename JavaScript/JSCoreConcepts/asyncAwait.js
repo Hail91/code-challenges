e29965932b63bd81a4b0d211e26ea47f48cc25d7
Aaron <aarongillies88@gmail.com>
2020-06-18 15:50:45 -0400

    added practice exercise for eventloop

--- JavaScript/JSCoreConcepts/asyncAwait.js
+++ JavaScript/JSCoreConcepts/asyncAwait.js
@@ -17,17 +17,28 @@ function processRequest(response) {
 }
 
 // Utilize Make Request/Process request functions
-makeReq("Google")
-  .then((res) => {
-    console.log("Request received!");
-    // Want to return the process request function now..
-    // And pass it the response from the makeReq Function
-    return processRequest(res);
-  }) // Need second then statement since makeReq returns a promise
-  .then((resAfterProcess) => {
-    // Log the response after processing
-    console.log(resAfterProcess);
-  }) // Catch if the nested promise rejects
-  .catch((error) => {
-    console.log(error);
-  });
+// makeReq("Google")
+//   .then((res) => {
+//     console.log("Request received!");
+//     // Want to return the process request function now..
+//     // And pass it the response from the makeReq Function
+//     return processRequest(res);
+//   }) // Need second then statement since makeReq returns a promise
+//   .then((resAfterProcess) => {
+//     // Log the response after processing
+//     console.log(resAfterProcess);
+//   }) // Catch if the nested promise rejects
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// Async/Await implementation
+
+async function doStuff() {
+  const response = await makeReq("Google");
+  console.log("Response Received!");
+  const processedRes = await processRequest(response);
+  console.log(processedRes);
+}
+
+doStuff();
---
+++ JavaScript/JSCoreConcepts/eventLoop.js
@@ -0,0 +1,12 @@
+// Event Loop example in JavaScript
+function eventLoopExample() {
+  console.log("alpha");
+  setTimeout(console.log("beta"), 0);
+  console.log("gamma");
+}
+
+// Should be alpha, gamma, beta
+// Beta returns last as the call stack needs to finish executing functions in the global execution
+// context before beta gets added to the stack
+
+console.log(eventLoopExample());
