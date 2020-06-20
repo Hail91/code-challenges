55740eeb054d780a1a7644c86edf8f4d1b53f538
Aaron <aarongillies88@gmail.com>
2020-06-20 19:02:05 -0400

    added questionTwo

--- JavaScript/TrickyJSConcepts/questionOne.js
+++ JavaScript/TrickyJSConcepts/questionOne.js
@@ -2,10 +2,14 @@
 function foo() {
   // b is neither declared in the global scope of the script, or in the function scope. So JS automatically assumes it to be global.
   let a = (b = 0);
+  // The above evaluates to
+  // let a;
+  // window.b = 0
+  // a = window.b
   a++;
   return a;
 }
 
 foo();
-typeof a; // => Will evaluate to undefined because a only exists in foo's immediate function scope
-typeof b; // => Will evaluate to 'Number' because b exists in the global scope.
+console.log(typeof a); // => Will evaluate to undefined because a only exists in foo's immediate function scope
+console.log(typeof b); // => Will evaluate to 'Number' because b exists in the global scope.
---
+++ JavaScript/TrickyJSConcepts/questionTwo.js
@@ -0,0 +1,4 @@
+const clothes = ["jacket", "t-shirt"];
+clothes.length = 0;
+
+console.log(clothes[0]); // => Undefined? Does it reduce the array to empty? <---- YES.
