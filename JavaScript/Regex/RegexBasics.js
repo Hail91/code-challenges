976b16e5ff49e460ff5d9528e9555fa36e552801
Aaron <aarongillies88@gmail.com>
2020-06-24 15:56:04 -0400

    added more regex content to file

--- JavaScript/Regex/RegexBasics.js
+++ JavaScript/Regex/RegexBasics.js
@@ -3,3 +3,9 @@
 // let quoteSample = "3 blind mice.";
 // let myRegex = /[^aeiou0-9]/gi;  <---- Match characters/Symbols not belonging to this charset
 // let result = quoteSample.match(myRegex);
+
+// ** Match Characters that Occur One or More Times **
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/g; // Change this line
+// let result = difficultSpelling.match(myRegex);
