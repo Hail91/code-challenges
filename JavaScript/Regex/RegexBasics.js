0faa84b8f698fdaa5716507c601b055a5a29d60c
Aaron <aarongillies88@gmail.com>
2020-06-24 15:32:37 -0400

    added regex problem to file

---
+++ JavaScript/Regex/RegexBasics.js
@@ -0,0 +1,5 @@
+// ** Match single Characters not specified **
+
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^aeiou0-9]/gi;  <---- Match characters/Symbols not belonging to this charset
+// let result = quoteSample.match(myRegex);
