a4a3e1b67e1a8d11bade0bceda8642344ddd1e90
Aaron <aarongillies88@gmail.com>
2020-07-14 15:23:55 -0400

    updated timeconversion to cover noon/midnight edge cases

--- JavaScript/timeConversion.js
+++ JavaScript/timeConversion.js
@@ -10,16 +10,25 @@ function timeConversion(s) {
   let seconds;
   // If PM is true, we need to do conversion for that
   if (PM) {
-    // Convert hours into 12 hour format. (ParseInt will target first element of
-    // newly created s array, then we use base 10 radix)
-    hours = 12 + parseInt(s[0], 10);
-    // Seconds stays the same, but we must remove the PM from the end using replace.
-    seconds = s[2].replace("PM", "");
+    if (s[0] !== "12") {
+      // Convert hours into 12 hour format. (ParseInt will target first element of
+      // newly created s array, then we use base 10 radix)
+      hours = 12 + parseInt(s[0], 10);
+      // Seconds stays the same, but we must remove the PM from the end using replace.
+      seconds = s[2].replace("PM", "");
+    } else {
+      return s[0] + ":" + s[1] + ":" + s[2].replace("PM", "");
+    }
   }
   // Otherwise, do the AM conversion.
   else {
-    hours = s[0];
-    seconds = s[2].replace("AM", "");
+    if (s[0] === "12") {
+      hours = s[0].replace("12", "00");
+      seconds = s[2].replace("AM", "");
+    } else {
+      hours = s[0];
+      seconds = s[2].replace("AM", "");
+    }
   }
   let newFormat = hours + ":" + minutes + ":" + seconds;
   return newFormat;
