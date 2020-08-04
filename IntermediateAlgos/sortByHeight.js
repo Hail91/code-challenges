63d395baa88143b84871e953076564a1aa5daf7c
Aaron <aarongillies88@gmail.com>
2020-08-04 12:19:02 -0400

    added solution for sortByHeight

---
+++ IntermediateAlgos/sortByHeight.js
@@ -0,0 +1,27 @@
+// Sort input Array ascending without modifying or moving -1 values.
+function sortByHeight(a) {
+  let tempArray = [];
+  // Loop over 'a' array
+  for (let i = 0; i < a.length; i++) {
+    // Check for values that are greater than or equal to zero and push them to tempArray
+    if (a[i] >= 0) {
+      tempArray.push(a[i]);
+    } else {
+      continue;
+    }
+  }
+  tempArray.sort((a, b) => a - b);
+  // Loop over a again replacing elements not -1 as we go
+  for (let j = 0; j < a.length; j++) {
+    if (a[j] >= 0) {
+      let min = Math.min(...tempArray);
+      a.splice(j, 1, min);
+      tempArray.splice(0, 1);
+    } else {
+      continue;
+    }
+  }
+  return a;
+}
+
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
