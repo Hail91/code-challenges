dab698cd9b081d362633f5e3ee077c2b8d41e3fb
Aaron <aarongillies88@gmail.com>
2020-08-13 13:32:55 -0400

    added correct solution for boxBlur

--- IntermediateAlgos/boxBlur.js
+++ IntermediateAlgos/boxBlur.js
@@ -1,27 +1,16 @@
 function boxBlur(image) {
-  // Need to access every 3 values across of each subArray while subArray[i] + 2 is NOT undefined, AND while there are multiples of 3 values available in each SubArray
-  // This should give us all possible 3x3 squares.
-  // Will need to keep track of number of pixels we're dealing with, as well as a sum of pixel values so we can do some math at the end to calculate the final integer result.
-  let runningSum = 0;
-  let tempSum = 0;
-  let numPixels = 0;
-  for (let i = 0; i < image.length; i++) {
-    // While image[i] + 2 is NOT undefined
-    while (image[i + 1] !== undefined) {
-      // Loop over SubArray
-      for (let j = 0; j < image[i].length; j++) {
-        // If j + 2 is NOT undefined, perform necessary logic
-        while (image[i][j + 2] !== undefined) {
-          runningSum = image[i][j] + image[i][j + 1] + image[i][j + 2];
-          tempSum += runningSum;
-          numPixels += 3;
-          break;
-        }
-      }
-      break;
+  let blurred = [];
+  for (let i = 1; i < image.length - 1; i++) {
+    let row = [];
+    for (let j = 1; j < image[i].length - 1; j++) {
+      let average = image[i - 1][j - 1] + image[i - 1][j] + image[i - 1][j + 1];
+      average += image[i][j - 1] + image[i][j] + image[i][j + 1];
+      average += image[i + 1][j - 1] + image[i + 1][j] + image[i + 1][j + 1];
+      row.push(Math.floor(average / 9));
     }
+    blurred.push(row);
   }
-  return Math.floor(runningSum / numPixels);
+  return blurred;
 }
 
 console.log(
