66146cae5d4061fa42a045b9aa358433aeabb963
Aaron <aarongillies88@gmail.com>
2020-09-19 14:30:10 -0400

    added solution for arithmetic progression

---
+++ Basic algorithm scripting/arithmeticProgression.js
@@ -0,0 +1,13 @@
+var canMakeArithmeticProgression = function (arr) {
+  // Sort input array
+  arr = arr.sort((a, b) => a - b);
+  // Get initial difference between 2 values
+  let diff = arr[1] - arr[0];
+  // Loop array and check for that value between every array pair
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] - arr[i] !== diff) return false;
+  }
+  return true;
+};
+
+console.log(canMakeArithmeticProgression([3, 5, 1]));
