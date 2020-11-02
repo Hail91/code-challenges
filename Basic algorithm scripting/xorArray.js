855f56e34defe8f44bf3ad5bb279104cdd808f7f
Aaron <aarongillies88@gmail.com>
2020-11-02 09:17:58 -0500

    added solution for xorArray

---
+++ Basic algorithm scripting/xorArray.js
@@ -0,0 +1,9 @@
+const xorOperation = function (n, start) {
+  let nums = [];
+  for (let i = 0; i < n; i++) {
+    nums.push(start + 2 * i);
+  }
+  return nums.reduce((a, b) => a ^ b);
+};
+
+console.log(xorOperation(4, 3));
