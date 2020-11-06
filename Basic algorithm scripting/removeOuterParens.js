8af62f825687b45b5376ce7854fa20118464e861
Aaron <aarongillies88@gmail.com>
2020-11-06 10:18:10 -0500

    working solution for remove outer parens

--- Basic algorithm scripting/removeOuterParens.js
+++ Basic algorithm scripting/removeOuterParens.js
@@ -1,21 +1,22 @@
-const removeOuterParentheses = function (S) {
+var removeOuterParentheses = function (S) {
   // Need to track where the outermost parenthese is from the starter
   let parensTracker = 0;
+  // Array on indexes to be removed
+  let parensArray = [];
+  // NewStr to append to
   let newStr = S.split("");
   // Loop over String
   for (let i = 0; i <= S.length; i++) {
-    if (i === S.length) break;
-    // If parensTracker is 0
-    if (parensTracker === 0 && i === 0) {
-      // remove char at i
-      newStr.splice(i, i + 1);
-      // Increment tracker
+    if (i === 0) {
+      parensArray.push(i);
       parensTracker += 1;
       continue;
     }
-    if (parensTracker === 0 && i !== 0) {
+    // If parensTracker is 0
+    if (parensTracker === 0) {
       // remove char at i
-      newStr.splice(i - 2, 1);
+      parensArray.push(i - 1, i);
+      parensTracker += 1;
       continue;
     }
     if (parensTracker > 0 && S[i] === "(") parensTracker += 1;
@@ -23,7 +24,13 @@ const removeOuterParentheses = function (S) {
       parensTracker -= 1;
     }
   }
+  // Loop over Parens array and adjust S as needed
+  for (let j = 0; j < parensArray.length - 1; j++) {
+    newStr.splice(parensArray[j], 1);
+    newStr.unshift("-");
+  }
+  newStr.splice(0, parensArray.length - 1);
   return newStr.join("");
 };
 
-console.log(removeOuterParentheses("()()(())"));
+console.log(removeOuterParentheses("(()())(())(()(()))"));
