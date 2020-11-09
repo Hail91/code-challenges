323fbf0c32e21f60616d81854bba6c75d9a083b8
Aaron <aarongillies88@gmail.com>
2020-11-09 18:43:46 -0500

    modify alphabetic shift to have chained return to reduce confusion

--- Basic algorithm scripting/alphabeticShift.js
+++ Basic algorithm scripting/alphabeticShift.js
@@ -3,14 +3,17 @@ function alphabeticShift(inputString) {
   let alpha = [...Array(26)].map((val, i) =>
     String.fromCharCode(i + 65).toLowerCase()
   );
-  inputString = inputString.split("").map((char, index) => {
-    if (char === "z") {
-      return (char = "a");
-    } else {
-      return (char = alpha[alpha.indexOf(char) + 1]);
-    }
-  });
-  return inputString.join("");
+  // Simplify return into chained return
+  return inputString
+    .split("")
+    .map((char, index) => {
+      if (char === "z") {
+        return (char = "a");
+      } else {
+        return (char = alpha[alpha.indexOf(char) + 1]);
+      }
+    })
+    .join("");
 }
 
-console.log(alphabeticShift('crazy'))
+console.log(alphabeticShift("crazy"));
