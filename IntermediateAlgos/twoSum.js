f53c918c35abae49ce72c540e24cf299726de634
Aaron <aarongillies88@gmail.com>
2020-11-16 18:29:16 -0500

    add comments for twoSum

--- IntermediateAlgos/twoSum.js
+++ IntermediateAlgos/twoSum.js
@@ -1,20 +1,27 @@
 function twoNumberSum(array, targetSum) {
-  // Initialize two pointers
+  // Sort Array (This implementation only works if it is sorted)
   array.sort((a, b) => a - b);
+  // Initialize two pointers
   let left = 0;
   let right = array.length - 1;
   // While loop that checks values of pointers summed together
   while (left < right) {
+    // Determine sum of pointers
     let sum = array[left] + array[right];
+    // If sum equals targetSum
     if (sum === targetSum) {
+      // Return those values
       return [array[left], array[right]];
     }
+    // If Sum is less than the target
     if (sum < targetSum) {
+      // Increment to the next value in array (ascending since it cannot be less under these circumstances)
       left += 1;
-    }
-    if (sum > targetSum) {
-      right -= 1;
-    }
+      // Otherwise, shift the right pointer down (to the left, since it cannot be more than what we calculated sum to be)
+    } else right -= 1;
   }
+  // If no values add up to target, return empty array
   return [];
 }
+
+console.log(twoNumberSum([1, 3, 5, 7, 9], 14));
