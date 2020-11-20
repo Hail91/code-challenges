5289f46b76fd3ae737e3a14d9b3482296f2c5234
Aaron <aarongillies88@gmail.com>
2020-11-20 16:58:23 -0500

    added comments to bomb.js for better clarity

--- Basic algorithm scripting/bomb.js
+++ Basic algorithm scripting/bomb.js
@@ -1,6 +1,10 @@
 function bomb(str) {
+  // Replace any characters in the string that are not lower/upper case english letters with empty spaces.
   str = str.replace(/[^a-zA-Z\s]/g, "").toLowerCase();
+  // Split string on empty space
   let strArray = str.split(" ");
+  // If the array includes the word bomb, return 'Duck!!!', else return 'There is no bomb, relax.'
   return strArray.includes("bomb") ? "Duck!!!" : "There is no bomb, relax.";
 }
+// Log output
 console.log(bomb("There is a bomb."));
