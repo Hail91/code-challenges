600170f31fcf07bad6d8d8d30566a8a00ed54149
Aaron <aarongillies88@gmail.com>
2020-11-21 22:43:48 -0500

    added comments for better clarity and understanding of chunkArray problem set

--- Basic algorithm scripting/chunkArray.js
+++ Basic algorithm scripting/chunkArray.js
@@ -3,14 +3,18 @@ function chunkArrayInGroups(arr, size) {
   let tempArr = [];
   // While array length is equal to or greater than size....
   while (arr.length >= size) {
-    // Splice off 'size' elements from front
+    // Splice off 'size' number of elements from front of input array
     let removed = arr.splice(0, size);
+    // Push those elements to our output array
     tempArr.push(removed);
   }
+  // If array length is less than size input
   if (arr.length > 0) {
+    // Simply push the entire array to output array
     tempArr.push(arr);
   }
+  // Return output array
   return tempArr;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
