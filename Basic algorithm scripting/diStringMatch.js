5cf1333d336927db6e0073f5574eda78808a1a7f
Aaron <aarongillies88@gmail.com>
2020-11-25 10:18:27 -0500

    second pass at diStringMatch, reduced down to one loop

--- Basic algorithm scripting/diStringMatch.js
+++ Basic algorithm scripting/diStringMatch.js
@@ -1,14 +1,27 @@
+// const diStringMatch = function (S) {
+//   let result = [];
+//   let indexes = S.split("").map((val, i, arr) => {
+//     return i;
+//   });
+//   indexes.push(S.length);
+//   for (let i = 0; i <= S.length; i++) {
+//     if (S[i] === "I") {
+//       result.push(...indexes.splice(indexes.indexOf(Math.min(...indexes)), 1));
+//     } else
+//       result.push(...indexes.splice(indexes.indexOf(Math.max(...indexes)), 1));
+//   }
+//   return result;
+// };
+
+// console.log(diStringMatch("DDI"));
+
 const diStringMatch = function (S) {
   let result = [];
-  let indexes = S.split("").map((val, i, arr) => {
-    return i;
-  });
-  indexes.push(S.length);
-  for (let i = 0; i <= S.length; i++) {
+  // Can do it all in one pass by setting beginning and end pointers in our loop
+  for (let i = 0, start = 0, end = S.length; i <= S.length; i++) {
     if (S[i] === "I") {
-      result.push(...indexes.splice(indexes.indexOf(Math.min(...indexes)), 1));
-    } else
-      result.push(...indexes.splice(indexes.indexOf(Math.max(...indexes)), 1));
+      result.push(start++);
+    } else result.push(end--);
   }
   return result;
 };
