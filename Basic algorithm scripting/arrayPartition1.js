5971e615ec2a6edd1547e9b5d2671cec9c4c4a5e
Aaron <aarongillies88@gmail.com>
2020-11-30 09:41:17 -0500

    rewrote arrayPartition1 to utilize inbuilt sort, cleaner solution and easier to solve when sorted

--- Basic algorithm scripting/arrayPartition1.js
+++ Basic algorithm scripting/arrayPartition1.js
@@ -1,27 +1,17 @@
-// ** TRASH ATTEMPT, NOT WORKING **
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 const arrayPairSum = function (nums) {
-  let arrHash = {};
-  let start = 0;
-  let end = nums.length - 1;
-  // For every element in nums, populate a hashtable with a key/value: array
-  for (let i = 1; i < nums.length; i++) {
-    arrHash[i] = []; // 1: [1, 4], [2, 3] // 2: [1, 3], [2, 4] 3: [1, 2], [3, 1]
-    while (arrHash[i].length < nums.length / 2) {
-      // Need to push first valid pair set
-      if (end - i === 0) {
-        end -= 1;
-        arrHash[i].push(
-          [nums[start], nums[start + i]],
-          [nums[end], nums[end - 1]]
-        );
-      } else {
-        arrHash[i].push(
-          [nums[start], nums[start + i]],
-          [nums[end], nums[end - i]]
-        );
-      }
-    }
+  // Sort array
+  nums.sort((a, b) => a - b);
+  // instantiate variable to track min
+  let output = 0;
+  // Loop over nums two at a time since we're dealing with PAIRS. (2n)
+  for (let i = 0; i < nums.length; i += 2) {
+    output += nums[i];
   }
+  return output;
 };
 
 console.log(arrayPairSum([6, 2, 6, 5, 1, 2]));
