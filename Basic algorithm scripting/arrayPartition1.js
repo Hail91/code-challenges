7caa894fea76eb1ba8c265f0656d6afc4b475497
Aaron <aarongillies88@gmail.com>
2020-11-30 09:42:44 -0500

    added comments for clarity to arrayPairSum

--- Basic algorithm scripting/arrayPartition1.js
+++ Basic algorithm scripting/arrayPartition1.js
@@ -3,15 +3,17 @@
  * @return {number}
  */
 const arrayPairSum = function (nums) {
-  // Sort array
+  // Sort array to make grabbing minimum of pairs much simpler
   nums.sort((a, b) => a - b);
   // instantiate variable to track min
   let output = 0;
   // Loop over nums two at a time since we're dealing with PAIRS. (2n)
   for (let i = 0; i < nums.length; i += 2) {
+    // Append to output, we know we're always getting the minimum for each pair since array is sorted.
     output += nums[i];
   }
+  // Return after we parse the entire array
   return output;
 };
-
+// Log output
 console.log(arrayPairSum([6, 2, 6, 5, 1, 2]));
