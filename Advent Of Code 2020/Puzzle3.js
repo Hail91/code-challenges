13f79683218df61d42a615b9d01f92c63a2faaae
Aaron <aarongillies88@gmail.com>
2020-12-02 12:31:37 -0500

    completed part 2 of day 2 in advent of code

--- Advent Of Code 2020/Puzzle3.js
+++ Advent Of Code 2020/Puzzle3.js
@@ -40,4 +40,28 @@ const validPassword = (passwords) => {
   return valid;
 };
 
-console.log(validPassword(input));
+// Part two
+const validPasswordPartTwo = (passwords) => {
+  let valid = 0;
+  // Loop over array of strings
+  for (let i = 0; i < passwords.length; i++) {
+    // Variable to hold number of valid char positions
+    let validChars = 0;
+    // Split string into array
+    let criteria = passwords[i].split(" ");
+    // Loop that array^ and set the acceptable range, the value to check
+    let range = criteria[0].split("-");
+    // Positions to check for valToCheck
+    let positionOne = Math.min(...range);
+    let positionTwo = Math.max(...range);
+    // Value to compare to required positions (must exist in only one of the two possible positions but not both or none)
+    let valToCheck = criteria[1][0];
+    if (criteria[2][positionOne - 1] === valToCheck) validChars += 1;
+    if (criteria[2][positionTwo - 1] === valToCheck) validChars += 1;
+    // Check validChars to determine whether or not we increment 'valid' before return
+    validChars == 1 ? (valid += 1) : null;
+  }
+  return valid;
+};
+
+console.log(validPasswordPartTwo(input));
