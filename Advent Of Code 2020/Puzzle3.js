95b22d06ad08b694a0aab5c8cad95782082442f1
Aaron <aarongillies88@gmail.com>
2020-12-02 15:59:39 -0500

    refactored advent of code day 2 algorithm for clarity

--- Advent Of Code 2020/Puzzle3.js
+++ Advent Of Code 2020/Puzzle3.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
-// Array to store data
 const input = [];
-// Read Input data and push to global array to be called by validPassword function.
 try {
   const data = fs.readFileSync("./inputs/Puzzle3Input.txt", "utf-8");
   const lines = data.split(/\r?\n/);
-  // Loop over lines and push to input
   lines.forEach((line) => {
     input.push(line);
   });
@@ -15,23 +12,16 @@ try {
 
 const validPassword = (passwords) => {
   let valid = 0;
-  // Loop over array of strings
   for (let i = 0; i < passwords.length; i++) {
-    // Split string into array
     let criteria = passwords[i].split(" ");
-    // Loop that array^ and set the acceptable range, the value to check
-    let range = criteria[0].split("-");
-    // Set min and max for acceptable ranges
-    let min = Math.min(...range);
-    let max = Math.max(...range);
-    let valToCheck = criteria[1][0];
-    // Use Regex matching to find number of occurences of valToCheck in string
-    let matcher = new RegExp(valToCheck, "g");
+    range = criteria[0].split("-");
+    valToCheck = criteria[1][0];
+    matcher = new RegExp(valToCheck, "g");
     if (criteria[2].match(matcher) === null) continue;
     else {
       if (
-        criteria[2].match(matcher).length >= min &&
-        criteria[2].match(matcher).length <= max
+        criteria[2].match(matcher).length >= Math.min(...range) &&
+        criteria[2].match(matcher).length <= Math.max(...range)
       ) {
         valid += 1;
       }
@@ -43,22 +33,14 @@ const validPassword = (passwords) => {
 // Part two
 const validPasswordPartTwo = (passwords) => {
   let valid = 0;
-  // Loop over array of strings
   for (let i = 0; i < passwords.length; i++) {
-    // Variable to hold number of valid char positions
     let validChars = 0;
-    // Split string into array
-    let criteria = passwords[i].split(" ");
-    // Loop that array^ and set the acceptable range, the value to check
-    let range = criteria[0].split("-");
-    // Positions to check for valToCheck
-    let positionOne = Math.min(...range);
-    let positionTwo = Math.max(...range);
-    // Value to compare to required positions (must exist in only one of the two possible positions but not both or none)
-    let valToCheck = criteria[1][0];
-    if (criteria[2][positionOne - 1] === valToCheck) validChars += 1;
-    if (criteria[2][positionTwo - 1] === valToCheck) validChars += 1;
-    // Check validChars to determine whether or not we increment 'valid' before return
+    criteria = passwords[i].split(" ");
+    range = criteria[0].split("-");
+    valToCheck = criteria[1][0];
+
+    if (criteria[2][Math.min(...range) - 1] === valToCheck) validChars += 1;
+    if (criteria[2][Math.max(...range) - 1] === valToCheck) validChars += 1;
     validChars == 1 ? (valid += 1) : null;
   }
   return valid;
