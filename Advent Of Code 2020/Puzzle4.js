2d91a83a8b139fcc7589badf11f21aa3265673f3
Aaron <aarongillies88@gmail.com>
2020-12-03 20:31:59 -0500

    refactored puzzle4, now working as intended

--- Advent Of Code 2020/Puzzle4.js
+++ Advent Of Code 2020/Puzzle4.js
@@ -4,49 +4,31 @@ try {
   const data = fs.readFileSync("./inputs/Puzzle4Input.txt", "utf-8");
   const lines = data.split(/\r?\n/);
   lines.forEach((line) => {
-    input.push([line]);
+    input.push(line.split(""));
   });
 } catch (error) {
   console.log(error, "Error reading from file!");
 }
 
-const SledPath = (map) => {
-  let treeCoords = [];
-  rowPos = 0;
-  currRow = 0;
-  trees = 0;
-  // While there are still rows to traverse
-  while (currRow <= map.length) {
-    if (map[currRow] === undefined) break;
-    if (rowPos >= map[currRow][0].length - 3) {
-      if (map[currRow][0][rowPos] === "#") {
-        treeCoords.push([rowPos, currRow, "X"]);
-        trees += 1;
-        rowPos = Math.abs(map[currRow][0].length - (rowPos + 3));
-        // currRow += 2;
-        continue;
-      } else {
-        treeCoords.push([rowPos, currRow, "0"]);
-        rowPos = Math.abs(map[currRow][0].length - rowPos + 3);
-        // currRow += 2;
-        continue;
-      }
-    } else if (
-      map[currRow][0][rowPos] === "#" &&
-      rowPos < map[currRow][0].length - 3
-    ) {
-      treeCoords.push([rowPos, currRow, "X"]);
+const SledPath = (right, map) => {
+  // Set total area height, as well as width of each 'row' (they'll all be 31)
+  let height = map.length;
+  let width = map[0].length;
+  // Set variables to track number of trees and steps taken
+  let trees = 0;
+  let stepNumber = 0;
+  // loop 'down' the 'map/hill'
+  for (let row = 1; row < height; row++) {
+    // Increment stepNumber
+    stepNumber += 1;
+    // Use modulo to 'skip' back to the front of the row when we can't make another 3 steps to the right
+    let xPosition = (stepNumber * right) % width;
+    // Check for a tree at xPosition in our map/hill
+    if (map[row][xPosition] === "#") {
       trees += 1;
-      rowPos += 3;
-      currRow += 1;
-    } else {
-      treeCoords.push([rowPos, currRow, "0"]);
-      rowPos += 3;
-      currRow += 1;
-      continue;
     }
   }
   return trees;
 };
 
-console.log(SledPath(input));
+console.log(SledPath(3, input));
