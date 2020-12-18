9f7163f1097853da2b354c5d626bd5f5717ca969
Aaron <aarongillies88@gmail.com>
2020-12-18 15:32:55 -0500

    added selection sort, but think I implemented it wrong, need to refactor

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,9 +1,14 @@
 // Selection Sort here....
-const selectionSort = () => {
+const selectionSort = (array) => {
   // Maintain two subArrays, one 'unsorted', and another 'sorted'.
+  let sorted = [];
+  let unsorted = array.slice();
   // Start sorted array by grabbing smallest element from unsorted and pushing to front of sorted.
-  // Repeatedly find smallest element from unsorted and push to front of sorted
-  // Return sorted
+  for (let i = 0; i < array.length; i++) {
+    sorted.push(Math.min(...unsorted));
+    unsorted.splice(unsorted.indexOf(Math.min(...unsorted)), 1);
+  }
+  return sorted;
 };
 
 console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
