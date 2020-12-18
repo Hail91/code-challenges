0a3e03f3ec753ffdebd6d348274c673dac2058ba
Aaron <aarongillies88@gmail.com>
2020-12-18 14:48:38 -0500

    added instructions for selectionSort, just need to code it up

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,7 +1,9 @@
 // Selection Sort here....
 const selectionSort = () => {
-  // Need to find research on how this works since I cannot currently remember!
-  return null;
+  // Maintain two subArrays, one 'unsorted', and another 'sorted'.
+  // Start sorted array by grabbing smallest element from unsorted and pushing to front of sorted.
+  // Repeatedly find smallest element from unsorted and push to front of sorted
+  // Return sorted
 };
 
 console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
