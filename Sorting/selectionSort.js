92a75a37cd48f92f15bc23cf61b4352c78924cfa
Aaron <aarongillies88@gmail.com>
2020-12-19 19:40:56 -0500

    need to rewrite selectionSort

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,14 +1,16 @@
 // Selection Sort here....
-const selectionSort = (array) => {
-  // Maintain two subArrays, one 'unsorted', and another 'sorted'.
-  let sorted = [];
-  let unsorted = array.slice();
-  // Start sorted array by grabbing smallest element from unsorted and pushing to front of sorted.
-  for (let i = 0; i < array.length; i++) {
-    sorted.push(Math.min(...unsorted));
-    unsorted.splice(unsorted.indexOf(Math.min(...unsorted)), 1);
-  }
-  return sorted;
-};
+// const selectionSort = (array) => {
+//   // Maintain two subArrays, one 'unsorted', and another 'sorted'.
+//   let sorted = [];
+//   let unsorted = array.slice();
+//   // Start sorted array by grabbing smallest element from unsorted and pushing to front of sorted.
+//   for (let i = 0; i < array.length; i++) {
+//     sorted.push(Math.min(...unsorted));
+//     unsorted.splice(unsorted.indexOf(Math.min(...unsorted)), 1);
+//   }
+//   return sorted;
+// };
 
-console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
+// console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
+
+// ^^ Above implemention works but is not a proper selectionSort, will rewrite to only use one array.
