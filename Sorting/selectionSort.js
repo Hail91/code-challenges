8266745077640c742d9b7abb68eeca23c35c9db6
Aaron <aarongillies88@gmail.com>
2020-12-21 15:17:55 -0500

    fixed selectionSort

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -2,9 +2,9 @@
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     let windowArray = array.slice(i);
-    array.splice(array.indexOf(Math.min(...windowArray)), 1),
-      array.splice(i, 0, Math.min(...windowArray));
+    array.splice(array.indexOf(Math.min(...windowArray), i), 1);
+    array.splice(i, 0, Math.min(...windowArray));
   }
   return array;
 };
-console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
+console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10, 3]));
