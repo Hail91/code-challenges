7dfcca96e7a29efe74e158d36fe1e23cba830067
Aaron <aarongillies88@gmail.com>
2020-12-21 14:27:24 -0500

    selection sort finished using sliding window method

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,25 +1,10 @@
-// Selection Sort here....
-// const selectionSort = (array) => {
-//   // Maintain two subArrays, one 'unsorted', and another 'sorted'.
-//   let sorted = [];
-//   let unsorted = array.slice();
-//   // Start sorted array by grabbing smallest element from unsorted and pushing to front of sorted.
-//   for (let i = 0; i < array.length; i++) {
-//     sorted.push(Math.min(...unsorted));
-//     unsorted.splice(unsorted.indexOf(Math.min(...unsorted)), 1);
-//   }
-//   return sorted;
-// };
-
-// console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
-
-// ^^ Above implemention works but is not a proper selectionSort, will rewrite to only use one array.
-
+// Selection Sort here...
 const selectionSort = (array) => {
-  // Use sliding window to slice array and slice to grab the minimum value
-  // unshift the min value to the front of array
-  // Move the window and repeat.
-  // Do this until array is sorted.
+  for (let i = 0; i < array.length; i++) {
+    let windowArray = array.slice(i);
+    array.splice(array.indexOf(Math.min(...windowArray)), 1),
+      array.splice(i, 0, Math.min(...windowArray));
+  }
+  return array;
 };
-
 console.log(selectionSort([2, 5, 7, 1, 4, 3, 9, 6, 10]));
