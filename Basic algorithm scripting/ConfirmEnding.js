70d533e7e2c7fbf22f2e44fbdf3490ac42a1286d
Aaron <aarongillies88@gmail.com>
2020-12-27 18:32:35 -0500

    modify endsWith algo

--- Basic algorithm scripting/ConfirmEnding.js
+++ Basic algorithm scripting/ConfirmEnding.js
@@ -1,17 +1,16 @@
-// function confirmEnding(str, target) {
-//   // get length of target string
-//   let targetLen = target.length;
-//   // Get last targetLen chars of 'str'
-//   let slicedStr = str.slice(-targetLen);
-//   if (slicedStr === target) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
-// Another way to write same solution
 function confirmEnding(str, target) {
+  // get length of target string
+  let targetLen = target.length;
+  // Get last targetLen chars of 'str'
+  let slicedStr = str.slice(-targetLen);
+  if (slicedStr === target) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// Another way to write same solution
+function confirmEndingShort(str, target) {
   if (str.endsWith(target) == true) {
     return true;
   } else return false;
