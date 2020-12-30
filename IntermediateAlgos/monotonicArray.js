ad380a4c1a49dd8534b6297b1d3da0b2aa3fe52e
Aaron <aarongillies88@gmail.com>
2020-12-30 14:41:02 -0500

    added solution for isMonotonic

---
+++ IntermediateAlgos/monotonicArray.js
@@ -0,0 +1,11 @@
+function isMonotonic(array) {
+  let increasing = true;
+  decreasing = true;
+  for (let i = 0; i < array.length; i++) {
+    array[i] > array[i + 1] ? (increasing = false) : null;
+    array[i] < array[i + 1] ? (decreasing = false) : null;
+  }
+  return increasing || decreasing;
+}
+
+console.log(isMonotonic([-1, -5, 10]));
