499daffe02ceb64dd760635b23d792db382588c8
Aaron <aarongillies88@gmail.com>
2020-12-30 15:37:35 -0500

    changed input to Monotonic array to test

--- IntermediateAlgos/monotonicArray.js
+++ IntermediateAlgos/monotonicArray.js
@@ -8,4 +8,4 @@ function isMonotonic(array) {
   return increasing || decreasing;
 }
 
-console.log(isMonotonic([-1, -5, 10]));
+console.log(isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]));
