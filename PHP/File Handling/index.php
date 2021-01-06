d630391295b9bb05c3f39b166e9d80d27ca86916
Aaron <aarongillies88@gmail.com>
2021-01-06 10:58:43 -0500

    added ability for function to recursively read file directories and output data

--- PHP/File Handling/index.php
+++ PHP/File Handling/index.php
@@ -1,8 +1,21 @@
 <?php 
+function readFoldersAndFiles($directory) {
 // Scandir will scan a directory and return all files in said directory
-$files = scandir('../');
+$files = scandir($directory);
 // Loop over $files array and print to terminal
 foreach($files as $file) {
-    print_r($file);
-    echo "\n";
+    if($file[0] === '.') continue;
+    // Check if file is a directory, if so...call function recursively until we hit the bottom.
+    if(is_dir($directory . '/' . $file)) {
+        echo "$file - Directory";
+        echo "\n";
+        readFoldersAndFiles($directory . '/' . $file);
+    }
+    else {
+        print_r($file);
+        echo "\n";  
+    }
 };
+}
+// Call function
+readFoldersAndFiles('../');
