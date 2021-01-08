e4730fad91e5fbb2f62b67d07348b2b02ba71460
Aaron <aarongillies88@gmail.com>
2021-01-08 16:16:49 -0500

    need to figure out how to return proper array from kadanes

--- hardAlgos/KadanesAlgorithm.js
+++ hardAlgos/KadanesAlgorithm.js
@@ -2,15 +2,17 @@ function kadanesAlgorithm(array) {
   let maxEnding = array[0];
   let maxSoFar = array[0];
   let index = 1;
-  let start;
-  let end = array.length - 1;
   while (index < array.length) {
     let currValue = array[index];
     maxEnding = Math.max(currValue, maxEnding + currValue);
     maxSoFar = Math.max(maxSoFar, maxEnding);
+    console.log(
+      `currValue: ${currValue}, Max Ending: ${maxEnding}, MaxSoFar: ${maxSoFar}`
+    );
     index += 1;
   }
-  return maxSoFar;
 }
 
-console.log(kadanesAlgorithm([1, -3, 2, 1, 3]));
+console.log(
+  kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4])
+);
