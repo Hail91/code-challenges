86c1d299ff1d5e827d2de74aaab955fde4c59ee8
Aaron <aarongillies88@gmail.com>
2021-01-09 22:13:17 -0500

    updated firstDuplicate

--- AlgoExpert/FirstDuplicate.js
+++ AlgoExpert/FirstDuplicate.js
@@ -1,16 +1,16 @@
-// function firstDuplicateValue(array) {
-//   let DuplicateHash = {};
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] in DuplicateHash) {
-//       DuplicateHash[array[i]] += 1;
-//       if (DuplicateHash[array[i]] == 2) {
-//         return array[i];
-//       }
-//     } else DuplicateHash[array[i]] = 1;
-//   }
-//   return -1;
-// }
-// console.log(firstDuplicateValue([2, 1, 5, 2, 3, 3, 4]));
+function firstDuplicateValueHash(array) {
+  let DuplicateHash = {};
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] in DuplicateHash) {
+      DuplicateHash[array[i]] += 1;
+      if (DuplicateHash[array[i]] == 2) {
+        return array[i];
+      }
+    } else DuplicateHash[array[i]] = 1;
+  }
+  return -1;
+}
+console.log(firstDuplicateValueHash([2, 1, 5, 2, 3, 3, 4]));
 
 // Alternative solution using O(1) space
 function firstDuplicateValue(array) {
