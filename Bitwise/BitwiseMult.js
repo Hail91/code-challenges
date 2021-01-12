a3c7dfe61abb0dcfa114c30f15b0fe4d1f1178ca
Aaron <aarongillies88@gmail.com>
2021-01-12 16:50:02 -0500

    added basic example of using bitshift to multiply a number

---
+++ Bitwise/BitwiseMult.js
@@ -0,0 +1,8 @@
+function bitShiftMultiply(num) {
+  return (num << 2) + num;
+}
+// Log output
+console.log(bitShiftMultiply(3));
+
+// 00000000000000000000000000001100
+// 00000000000000000000000000000001
