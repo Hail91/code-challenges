40c4bfda75924a33d96932c343074349a145bac6
Aaron <aarongillies88@gmail.com>
2021-02-01 17:07:13 -0500

    added comments for factorialize recursive

--- Basic algorithm scripting/factorialize.js
+++ Basic algorithm scripting/factorialize.js
@@ -1,9 +1,11 @@
+// Get factorial of input number using recursion
 function factorialize(num) {
   if (num === 0) {
     return 1;
   } else {
+    // 5 * 4 * 3 * 2 * 1
     return num * factorialize(num - 1);
   }
 }
-
+// Log output
 factorialize(5);
