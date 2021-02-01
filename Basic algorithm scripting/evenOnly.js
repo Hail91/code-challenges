12fa98f2dd806fa9d1ca500b6988f962834bfe6d
Aaron <aarongillies88@gmail.com>
2021-02-01 17:05:47 -0500

    comments for clarity on evenDigitsOnly

--- Basic algorithm scripting/evenOnly.js
+++ Basic algorithm scripting/evenOnly.js
@@ -1,8 +1,12 @@
 function evenDigitsOnly(n) {
   n = n
+    // Convert to string
     .toString()
+    // Split into array
     .split("")
+    // Map over array, if value is even return true, else return false.
     .map((el) => (el % 2 === 0 ? true : false));
+  // Check if every result is even, if so return true, otherwise return false
   return n.every((el) => el === true);
 }
 
