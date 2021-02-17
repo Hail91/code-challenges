41a3c1389b16f99cd2f863b17fd14642378e0ef9
Aaron <aarongillies88@gmail.com>
2021-02-17 11:33:42 -0500

    updated absValue/codeSignal verbiage

--- codeSignal/absoluteSumMinimization.js
+++ codeSignal/absoluteSumMinimization.js
@@ -23,10 +23,10 @@ function absoluteValuesSumMinimization(array) {
   );
 }
 // Log output
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
 
 function absoluteValuesSumMinimizationRefactor(Array) {
   return Array[Math.ceil(Array.length / 2) - 1];
 }
-
+// Log output
 console.log(absoluteValuesSumMinimizationRefactor([2, 4, 7, 9, 10, 14, 15]));
