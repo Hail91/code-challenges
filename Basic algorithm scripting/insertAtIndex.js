c38fc77faf26332da2fb94b3acc9b995020ad862
Aaron <aarongillies88@gmail.com>
2021-02-18 17:33:36 -0500

    minor formatting changes to insertAtIndex

--- Basic algorithm scripting/insertAtIndex.js
+++ Basic algorithm scripting/insertAtIndex.js
@@ -1,30 +1,23 @@
 function getIndexToIns(arr, num) {
-  let valIndex;
+  let valIndex = 0;
   // sort input array
   arr = arr.sort((a, b) => a - b);
   // If array is empty, insert at 0th (first index)
-  if (arr.length === 0) {
-    valIndex = 0;
-    return valIndex;
-    // Otherwise, if num is greater than last value in array(since it's sorted)
-  } else if (num >= arr[arr.length - 1]) {
+  if (arr.length === 0) return valIndex;
+  else if (num >= arr[arr.length - 1]) {
     // Insert in last position (current index of largest + 1)
     valIndex = arr.length;
     return valIndex;
   }
   // Loop over sorted input array
   for (let i = 0; i < arr.length; i++) {
-    // if Array is empty, insert at index 0
-    // compare num to each value of array checking whether it fits in
     if (num > arr[i] && num < arr[i + 1]) {
       valIndex = arr.indexOf(arr[i]) + 1;
       return valIndex;
     } else if (num <= arr[i] && num < arr[i + 1]) {
       valIndex = arr.indexOf(arr[i]);
       return valIndex;
-    }
-    // If not, continue iterating through until you find a place for it.
-    else continue;
+    } else continue;
   }
 }
 // Log output
