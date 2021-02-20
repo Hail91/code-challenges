d71bda25ee51352997b813556c96056504b3c288
Aaron <aarongillies88@gmail.com>
2021-02-20 15:06:31 -0500

    refactored largestOfFour into clean .map solution versus verbose for loop

--- Basic algorithm scripting/largestOfFour.js
+++ Basic algorithm scripting/largestOfFour.js
@@ -1,16 +1,14 @@
 function largestOfFour(arr) {
-  let newArr = [];
-  // Iterate through subArrays getting Max value, push that value to a newArray, return the newArray
-  for (let i = 0; i < arr.length; i++) {
-    let maxVal = Math.max(...arr[i]);
-    newArr.push(maxVal);
-  }
-  return newArr;
+  return arr.map((element) => {
+    return Math.max(...element);
+  });
 }
 
-largestOfFour([
-  [4, 5, 1, 3],
-  [13, 27, 18, 26],
-  [32, 35, 37, 39],
-  [1000, 1001, 857, 1],
-]);
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
