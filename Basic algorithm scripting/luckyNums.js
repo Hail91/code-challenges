ef76e8e9a7cd58c201cc765c292cb45393a72daa
Aaron <aarongillies88@gmail.com>
2021-03-16 17:28:50 -0400

    added comments for clarity to luckyNumbers

--- Basic algorithm scripting/luckyNums.js
+++ Basic algorithm scripting/luckyNums.js
@@ -1,5 +1,8 @@
 var luckyNumbers = function (matrix) {
+  // Map over Matrix of numbers returning an array of the minimum from each matrix subArray
   let min = matrix.map((x) => Math.min(...x));
+  // Repeat for first Subarray of matrix, returning the max of each value from first SubArray Matrix
   let max = matrix[0].map((_, i) => Math.max(...matrix.map((x) => x[i])));
+  // Filter min by values that also exist in Max.
   return min.filter((x) => max.includes(x));
 };
