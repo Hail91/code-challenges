dc90aef07edd746335df569bc71b47c03fd148d8
Aaron <aarongillies88@gmail.com>
2021-03-24 20:59:01 -0400

    added comments for peakIndexMountainArray

--- Basic algorithm scripting/peakMountain.js
+++ Basic algorithm scripting/peakMountain.js
@@ -1,5 +1,6 @@
 var peakIndexInMountainArray = function (A) {
+  // Return the index of the tallest point of 'mountain' array
   return A.indexOf(Math.max(...A));
 };
-
+// Log output
 console.log(peakIndexInMountainArray([0, 1, 0]));
