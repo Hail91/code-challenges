3a2eee8af88fb7b4046329ba49de6de69d2167d3
Aaron <aarongillies88@gmail.com>
2021-03-31 19:05:47 -0400

    added faster solution for squaresSorted

--- Basic algorithm scripting/squaresSorted.js
+++ Basic algorithm scripting/squaresSorted.js
@@ -1,8 +1,29 @@
-var sortedSquares = function (A) {
-  A.forEach((el, index, arr) => {
-    return (arr[index] = Math.pow(el, 2));
-  });
-  return A.sort((a, b) => a - b);
-};
+// var sortedSquares = function (A) {
+//   A.forEach((el, index, arr) => {
+//     return (arr[index] = Math.pow(el, 2));
+//   });
+//   return A.sort((a, b) => a - b);
+// };
+// // Log output
+// console.log(sortedSquares([-4, -1, 0, 3, 10]));
 
+function sortedSquares(array) {
+  const output = [];
+  let leftPointer = 0,
+    rightPointer = array.length - 1,
+    length = array.length - 1;
+  while (length >= 0) {
+    if (Math.abs(array[leftPointer]) > Math.abs(array[rightPointer])) {
+      output[length] = array[leftPointer] * array[leftPointer];
+      leftPointer += 1;
+      length -= 1;
+    } else {
+      output[length] = array[rightPointer] * array[rightPointer];
+      rightPointer -= 1;
+      length -= 1;
+    }
+  }
+  return output;
+}
+// Log output
 console.log(sortedSquares([-4, -1, 0, 3, 10]));
