400ae5807b9044af6dda51f8bcef9efbfbbeba89
Aaron <aarongillies88@gmail.com>
2021-04-05 17:02:21 -0400

    commented sayHelloBye

--- Basic algorithm scripting/sayHelloBye.js
+++ Basic algorithm scripting/sayHelloBye.js
@@ -1,7 +1,9 @@
 function sayHelloBye(name, num) {
+  // Update Name Formatting
   name = name.charAt(0).toUpperCase() + name.substring(1);
+  // Return Hello `name` if num == 1, else return Bye.
   if (num === 1) return `Hello ${name}`;
   else return `Bye ${name}`;
 }
-
+// Log output
 console.log(sayHelloBye("John", 1));
