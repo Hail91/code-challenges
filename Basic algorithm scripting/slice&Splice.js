f07487c43f52984aac1a7f9a0c1ba43723c6a5b8
Aaron <aarongillies88@gmail.com>
2021-04-06 16:19:42 -0400

    commented slice&splice

--- Basic algorithm scripting/slice&Splice.js
+++ Basic algorithm scripting/slice&Splice.js
@@ -1,9 +1,14 @@
 function frankenSplice(arr1, arr2, n) {
+  // Create copy of array2
   let newArr = arr2.slice(0);
+  // Iterate over array1
   for (let i = 0; i < arr1.length; i++) {
+    // Insert i'th element from array1 to newArr starting at index 1
     newArr.splice(n, 0, arr1[i]);
+    // Increment n
     n++;
   }
   return newArr;
 }
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+// Log output
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
