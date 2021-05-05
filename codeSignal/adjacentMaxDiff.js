bd05b79e9724d71682b1fa555ae5f890a16dc7d7
Aaron <aarongillies88@gmail.com>
2021-05-05 18:12:37 -0400

    add explanation for arrayMaximalAdjacentDifferce/codeSignal

--- codeSignal/adjacentMaxDiff.js
+++ codeSignal/adjacentMaxDiff.js
@@ -1,8 +1,13 @@
 function arrayMaximalAdjacentDifference(inputArray) {
+  // Map over the input array
   let adjacentMaxes = inputArray.map((num, index, array) => {
+    // If we're on the last element, simply return
     if (index === array.length - 1) return;
+    // Otherwise, return the absolute value between the current value and the next value
     return Math.abs(array[index] - array[index + 1]);
   });
+  // After we've mapped over the array calculating the differences between each value
+  // Filter out the last undefined value and return the greatest difference
   return Math.max(...adjacentMaxes.filter((num) => num !== undefined));
 }
 // Log output
