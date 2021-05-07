a7863b5d1e11a25e906930b26e9c4c8b68052574
Aaron <aarongillies88@gmail.com>
2021-05-07 13:44:38 -0400

    added explanation for allLongestStrings/codeSignal

--- codeSignal/allLongestStrings.js
+++ codeSignal/allLongestStrings.js
@@ -1,8 +1,10 @@
+// Return only the longest strings from the array
 function allLongestStrings(inputArray) {
+  // Map over the input array and return the lengths of every string in the array
   let strLens = inputArray.map((string) => {
     return string.length;
   });
-  // Filter inputArray by Max strLen
+  // Return values from the input array where the string length matches the maximum found length
   return inputArray.filter((str) => {
     return str.length === Math.max(...strLens);
   });
