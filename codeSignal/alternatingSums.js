24de2c1a2b9c3c881365cab2ec017e6789743d82
Aaron <aarongillies88@gmail.com>
2021-05-08 13:50:15 -0400

    added comments for alternatingSums/codeSignal

--- codeSignal/alternatingSums.js
+++ codeSignal/alternatingSums.js
@@ -1,17 +1,21 @@
 function alternatingSums(a) {
+  // Initialize variables to track scores for both teams and the current state switch
   let team1 = 0;
   team2 = 0;
   state = 1;
   // Loop over array
   for (let i = 0; i < a.length; i++) {
+    // If state is 'on', add to team 1's score
     if (state === 1) {
       team1 += a[i];
       state = 0;
+      // Otherwise, add to team 2's score
     } else {
       team2 += a[i];
       state = 1;
     }
   }
+  // Return both scores
   return [team1, team2];
 }
 // Log output
