4b064b3bbe5d2511fa291778b5882517718004d6
Aaron <aarongillies88@gmail.com>
2021-05-10 16:59:25 -0400

    updated code and comments for arrayMaxConsecutiveSum/codeSignal

--- codeSignal/arrayMaxConsecutiveSum.js
+++ codeSignal/arrayMaxConsecutiveSum.js
@@ -1,15 +1,27 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
+  // If the passed value for 'k' exceeds the array length, it's not possible to calculate a consectiveSum of that length
+  if (k > inputArray.length) {
+    return 0;
+  }
+  // Track maxSum so we have something to compare the currentSum against
   let maxSum = 0;
+  // Keep a count variable that we use to determine when we've hit our desired iterations inside loop
   let count = 0;
+  // Loop over array while we have at least k more elements to hit
   for (let i = 0; i + k <= inputArray.length; i++) {
-    let sum = inputArray[i];
+    // Track the currentSum in the loop
+    let currentSum = inputArray[i];
+    // Run inner loop 'k' times and calculate the sum on each pass
     while (count < k - 1) {
       count += 1;
-      sum += inputArray[i + count];
+      currentSum += inputArray[i + count];
     }
-    if (sum > maxSum) maxSum = sum;
+    // If our calculated currentSum is > than maxSum, reassign maxSum
+    if (currentSum > maxSum) maxSum = currentSum;
+    // Reset count for next iteration (count is a temporary variable that the inner loop relies on)
     count = 0;
   }
+  // return the maximum calculated sum
   return maxSum;
 }
 // Log output
