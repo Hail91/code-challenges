1530e07f4718f78a45eb2ce0230d7a85f6890a21
Aaron <aarongillies88@gmail.com>
2021-05-17 11:20:51 -0400

    cleaned up code in generateDocument/algoExpert

--- AlgoExpert/generateDocument.js
+++ AlgoExpert/generateDocument.js
@@ -1,20 +1,20 @@
-// Time Complexity: O(n + m) where n is the number of characters and m is the length of document
-// Space Complexity: O(n) where 'n' is number of characters in characters string
+// Space Complexity: O(n + m) where n is the number of characters and m is the length of document
+// Time Complexity: O(n) where 'n' is number of characters in characters string
 function generateDocument(characters, document) {
   let freqHash = {};
   // Fill a hashtable with frequencies from characters string
-  for (let i = 0; i < characters.length; ++i) {
+  for (char of characters) {
     // If character is already in hashTable, increment freq
-    if (characters[i] in freqHash) freqHash[characters[i]] += 1;
+    if (char in freqHash) freqHash[char] += 1;
     // Otherwise, add it
-    else freqHash[characters[i]] = 1;
+    else freqHash[char] = 1;
   }
   // Loop the document string and reduce from hashTable freqs
-  for (let j = 0; j < document.length; ++j) {
+  for (c of document) {
     // If we go below zero, we don't have enough chars, return false
-    if (document[j] in freqHash) {
-      freqHash[document[j]] -= 1;
-      if (freqHash[document[j]] < 0) return false;
+    if (c in freqHash) {
+      freqHash[c] -= 1;
+      if (freqHash[c] < 0) return false;
     } else return false;
   }
   // If we make it through entire loop, return true
