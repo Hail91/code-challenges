fe975b4e81103649ba6647303b2659e21266e0ed
Aaron <aarongillies88@gmail.com>
2021-07-03 16:32:02 -0400

    commented nonConstructibleChange/algoExpert

--- AlgoExpert/nonConstructibleChange.js
+++ AlgoExpert/nonConstructibleChange.js
@@ -1,15 +1,22 @@
 // Time Complexity: O(nLogn) worst case where n is the length of the coins array
 // Space Complexity: O(1) constant space, sort occurs in-place and we use a variable to track output
 function nonConstructibleChange(coins) {
+  // Set currentChange to zero
   let currentChange = 0;
+  // Sort change in ascending order
   coins.sort((a, b) => a - b);
+  // Loop over coins array
   for (let i = 0; i < coins.length; ++i) {
+    // If the value of current iteration in coins array is greater than currentChange + 1
     if (coins[i] > currentChange + 1) {
+      // Return currentChange + 1
       return currentChange + 1;
     } else {
+      // Otherwise update currentChange
       currentChange += coins[i];
     }
   }
+  // Return currentChange + 1
   return currentChange + 1;
 }
 // Log output
