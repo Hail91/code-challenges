6958cc544bbbd08cd9ff80b5e7dfd54365bb6c65
Aaron <aarongillies88@gmail.com>
2021-07-03 20:48:46 -0400

    added comments for add method currying example

--- JS Functional Programming/basicCurrying.js
+++ JS Functional Programming/basicCurrying.js
@@ -1,8 +1,12 @@
 function add(x) {
+  // Return a function which takes a parameter 'y'
   return function (y) {
+    // Return a function which takes a parameter 'z'
     return function (z) {
+      // Since we have access to x and y via closure, return x + y + z
       return x + y + z;
     };
   };
 }
-add(10)(20)(30);
+// Log output
+console.log(add(10)(20)(30));
