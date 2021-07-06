9cc30206f41acdf0621477c99b371b776f580b35
Aaron <aarongillies88@gmail.com>
2021-07-06 17:01:13 -0400

    added working solution for minWaysToMakeChange/algoExpert

--- AlgoExpert/minWaysToMakeChange.js
+++ AlgoExpert/minWaysToMakeChange.js
@@ -1,24 +1,27 @@
-/* NOT WORKING AS INTENDED */
+// Time complexity: O(nd) where n is the target int and d is the number of denoms
+// Space Complexity: O(n) where n is the target int (must construct array of length 'n')
 function minNumberOfCoinsForChange(n, denoms) {
-  let ways = 0;
-  let currentMax = -Infinity;
-  if (n === 0) return 0; // Zero ways to make zero cents
-  // Sort denoms array from largest denoms to smallest denoms
-  denoms.sort((a, b) => b - a);
-  // [10, 5, 1] n = 10
-  // Use the largest denoms first (as many as possible) until it's larger than our remaining change to make
-  for (let i = 0; i < denoms.length; i++) {
-    currentMax = denoms[i];
-    while (n >= currentMax) {
-      n -= currentMax;
-      ways += 1;
+  // Build array of length n + 1 (n + 1 because we must represent 0 dollars at first index)
+  let possibilities = Array.from(Array(n + 1), () => Infinity);
+  // Set the first value to be zero (zero ways to make zero dollars scenario)
+  possibilities[0] = 0;
+  // Iterate over denoms
+  for (let denom of denoms) {
+    // For each denom, iterate over the array of possible amounts
+    for (let amount = 0; amount < possibilities.length; amount += 1) {
+      // If the denom in question is less than or equal to the amount we're trying to make
+      if (denom <= amount) {
+        // Reassign the amount in our array to the smaller of the two values
+        // Either the current amount that can be used in the array, or 1 + the last calculation
+        possibilities[amount] = Math.min(
+          possibilities[amount],
+          1 + possibilities[amount - denom]
+        );
+      }
     }
-    if (n === 0) return ways;
-    // Move to next denom
-    else continue;
   }
-  // If we make it all the way to the smallest and cannot reach exactly zero, return -1
-  return -1;
+  // If change can be made, return that amount, otherwise return -1
+  return possibilities[n] !== Infinity ? possibilities[n] : -1;
 }
 // Log output
-console.log(minNumberOfCoinsForChange(9, [3, 5]));
+console.log(minNumberOfCoinsForChange(6, [1, 2, 4]));
