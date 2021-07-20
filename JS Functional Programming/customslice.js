5d123521ffc2756fdf378e77e5fad640aefe929d
Aaron <aarongillies88@gmail.com>
2021-07-20 17:02:05 -0400

    added explanation for slice example

--- JS Functional Programming/customslice.js
+++ JS Functional Programming/customslice.js
@@ -1,7 +1,13 @@
+// Slice is another array method that utilizes the concept of Function programming (pure functions)
+let inputArray = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
 function sliceArray(anim, beginSlice, endSlice) {
+  // Copy input array into a new array
   let newSlice = [...anim];
+  // Slice that array with passed indexes
   newSlice = newSlice.slice(beginSlice, endSlice);
+  // Return sliced copy
   return newSlice;
 }
-var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
-sliceArray(inputAnim, 1, 3);
+// Log output
+sliceArray(inputArray, 1, 3);
