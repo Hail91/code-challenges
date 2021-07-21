c3b431d0867bfd4b7412d8e7acd4e89da7aa3e04
Aaron <aarongillies88@gmail.com>
2021-07-21 11:14:44 -0400

    finished destructuring challenge

--- JS ES6/destructuring.js
+++ JS ES6/destructuring.js
@@ -5,16 +5,23 @@
 /* The target on left | The source on right */
 
 // Array destructuring
-var [first, second] = [true, false];
 let [first, second] = [true, false];
-const [first, second] = [true, false];
 
 // Object destructuring
-var { first, second } = { first: 0, second: 1 };
-let { first, second } = { first: 0, second: 1 };
-const { first, second } = { first: 0, second: 1 };
+let { firstObj, secondObj } = { first: 0, second: 1 };
 
 // Array Assignment
 [first, second] = [true, false];
 // Object Assignment
-let { first, second } = { first: 0, second: 1 };
+let { a, b } = { firstObj: 0, secondObj: 1 };
+
+/* Destructuring Challenge */
+const data = {
+  name: "Rusty",
+  room: "Kitchen",
+  weapon: "Candlestick",
+};
+const { room, weapon } = data;
+
+console.log(room, weapon);
+/* End */
