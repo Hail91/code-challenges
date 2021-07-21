06d27878d7f5c65aad64cf030069bee183cb0375
Aaron <aarongillies88@gmail.com>
2021-07-21 10:45:27 -0400

    started lesson on destructuring

---
+++ JS ES6/destructuring.js
@@ -0,0 +1,12 @@
+// Destructuring (added with ES6) allows us to extract pieces of data from objects (arrays / hashmaps)
+
+/* Variable declarations */
+
+/* The target on left | The source on right */
+var [first, second] = [true, false];
+let [first, second] = [true, false];
+const [first, second] = [true, false];
+
+// Assignment
+
+[first, second] = [true, false];
