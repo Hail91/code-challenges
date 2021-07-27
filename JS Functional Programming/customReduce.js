be110af010bfc6a00afdd3f5efcc4e935078c6e5
Aaron <aarongillies88@gmail.com>
2021-07-27 11:19:30 -0400

    added fully worked out solution for more advanced reduce example by going under the hood a bit

--- JS Functional Programming/customReduce.js
+++ JS Functional Programming/customReduce.js
@@ -4,3 +4,23 @@ let sum = [1, 2].reduce((sum, value) => {
 }, 0);
 // Log output
 console.log(sum, "Sum of all values");
+
+// Callback Fn for below
+function add(val, accumulator) {
+  return val + accumulator;
+}
+
+// More advanced implementation
+let advancedReduce = (list, callback, initialValue) => {
+  // Set the initial value
+  let acc = initialValue;
+  // Loop through list
+  for (let i = 0; i < list.length; i++) {
+    // execute callback (add) with current value and the accumulator and assign to accumulator
+    acc = callback(list[i], acc);
+  }
+  // Return the accumulator
+  return acc;
+};
+// Log the output
+console.log(advancedReduce([1, 2, 3], add, 0));
