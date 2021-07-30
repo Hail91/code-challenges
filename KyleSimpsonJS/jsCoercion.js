ef4ca6412795f9a6ffd688577ef520f49b15702e
Aaron Gillies <aarongillies88@gmail.com>
2021-07-30 12:15:33 -0400

    added more examples of Math operations, but this time using ONLY string values

--- KyleSimpsonJS/jsCoercion.js
+++ KyleSimpsonJS/jsCoercion.js
@@ -9,3 +9,28 @@ console.log(exampleOne, "Subract number from a string");
     There is no situation where you can subtract from a string, so JS internally 'coerces' the string to a number by calling toPrimitive --> toNumber. In the end this ends up 
     being 2 - 2 which results in the number zero being returned.
 */
+
+// More math operations against a string value type
+
+// Multiplication
+let exampleTwo = "2" * 2;
+console.log(exampleTwo, "Multiple string by a number");
+// Division
+let exampleThree = "2" / 2;
+console.log(exampleThree, "Divide string by a number");
+/*
+    The above examples behave similarly to exampleOne, exampleTwo will return 4 with exampleThree returning 1. The reason for this is that both multiplication and division are mathematical operations,
+    when JS sees them, it coerces the left hand string value to a number before execution. Again, it will call toPrimitive on the string, and then toNumber before doing the math.
+*/
+
+// The above examples return the same output when doing these operations with JUST strings...examples below...
+let exampleOneStrings = "2" - "2";
+let exampleTwoStrings = "2" * "2";
+let exampleThreeStrings = "2" / "2";
+// Log output
+console.log(
+  exampleOneStrings,
+  exampleTwoStrings,
+  exampleThreeStrings,
+  "Examples executing Math operations on JUST strings"
+);
