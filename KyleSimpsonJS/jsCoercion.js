e0d7ecd2cf5e2ba14b5cabacf84c34f0ef9bc1da
Aaron Gillies <aarongillies88@gmail.com>
2021-07-30 12:43:03 -0400

    added final example for type checking/coercion

--- KyleSimpsonJS/jsCoercion.js
+++ KyleSimpsonJS/jsCoercion.js
@@ -64,6 +64,23 @@ function isValidName(name) {
   return false;
 }
 
-function hoursAttended() {
-  return null;
+function hoursAttended(attended, length) {
+  if (typeof attended == "string" && attended.trim() !== "") {
+    attended = Number(attended);
+  }
+  if (typeof length == "string" && length.trim() !== "") {
+    length = Number(length);
+  }
+  if (
+    typeof attended == "number" &&
+    typeof length == "number" &&
+    attended >= 0 &&
+    length >= 0 &&
+    Number.isInteger(attended) &&
+    Number.isInteger(length) &&
+    attended <= length
+  ) {
+    return true;
+  }
+  return false;
 }
