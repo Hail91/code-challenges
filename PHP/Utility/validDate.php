f380acec1a760e2675343aba168a1dabf5a3a5a5
Aaron Gillies <aarongillies88@gmail.com>
2021-08-07 16:50:20 -0400

    added comments for PHP valid date function

--- PHP/Utility/validDate.php
+++ PHP/Utility/validDate.php
@@ -1,14 +1,19 @@
 <?php
 function valid_sql_date($date) {
+  // Split the date on hyphens (e.g 1994-10-02)
   $splitDate = explode("-", $date);
+  // Loop over that array 
   for($i = 0; $i < count($splitDate); $i++) {
+    // Check each value for truthiness
     $i == 0 && $splitDate[$i] == 4 && $i == 1 && $splitDate[$i] == 2 && $i == 2 && $splitDate[$i] == 2 ? true : false; 
   }
+  // Pass the dates into the checkdate PHP method
   if(checkdate($splitDate[1], $splitDate[2], $splitDate[0])) {
+    // Return true if it's valid
     return true;
   }
+  // Otherwise return false
   else return false;
-  return true;
 }
-
-echo valid_sql_date("1999-04-30");
+// Echo output
+echo valid_sql_date("1999-04-34"); // Would return nothing/falsy because April does not have 34 days
