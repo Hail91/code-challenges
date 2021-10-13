e5ad6f4cc97f605325f273228797b3d2a95b255d
Aaron Gillies <aarongillies88@gmail.com>
2021-10-13 18:27:20 -0400

    added solution for builtins as well

--- DailyByte/reverseString.js
+++ DailyByte/reverseString.js
@@ -1,4 +1,4 @@
-// Reverse a string
+// Reverse a string utilizing pointers
 function reverseString(string) {
   let strArray = string.split("");
   let temp;
@@ -11,7 +11,19 @@ function reverseString(string) {
   return strArray.join("");
 }
 
-// Test cases
+// Reverse using builtins
+function reverseStringBuiltin(string) {
+  return string.split("").reverse().join("");
+}
+
+// Add solution using recursion *to do*
+
+// Test cases for solution #1
 console.log(reverseString("cat"));
 console.log(reverseString("cart"));
 console.log(reverseString("Hello"));
+
+// Test cases for solution #2
+console.log(reverseStringBuiltin("cat"));
+console.log(reverseStringBuiltin("cart"));
+console.log(reverseStringBuiltin("Hello"));
