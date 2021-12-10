8a1d2b8e5a05bbcbd0be3f37cd579e8e6e3cf252
Aaron Gillies <aarongillies88@gmail.com>
2021-12-10 18:05:25 -0500

    added solution for AOC day 2 part 1

--- AOC2021/utils/index.js
+++ AOC2021/utils/index.js
@@ -5,11 +5,11 @@ const conversionMap = new Map([["integer", parseInt]]);
 
 export const getInputString = (relPath) => {
   // read the input file
-  const inputStr = fs.readFileSync("../day1/input.txt", "utf-8");
+  const inputStr = fs.readFileSync("../day2/input.txt", "utf-8");
   return inputStr;
 };
 
-export const getInputArray = (str, separator = "\n", type = "integer") => {
+export const getInputArray = (str, separator = "\n", type = "string") => {
   // convert input string into array
   const inputArr = str.split(separator);
 
